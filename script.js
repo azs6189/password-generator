885ab4083032053191ab7504962e7d03494f4cec
Allie Soliz <solizale@gmail.com>
2022-12-06 14:54:40 -0500

    added variables for password criteria

---
+++ script.js
@@ -0,0 +1,65 @@
+// Variables for password criteria
+
+let letters = [
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+];
+
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"];
+
+console.log("Welcome to the Password Generator!");
+let nr_letters = parseInt(
+	prompt("How many letters would you like in your password?")
+);
+console.log(nr_letters);
